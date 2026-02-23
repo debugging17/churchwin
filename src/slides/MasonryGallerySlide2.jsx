1e9c01429a564513a34ec16828b643c0a5cb93d8
Your Name <cephas.kudalor147@gmail.com>
2026-02-23 15:59:34 +0000

    style: dynamic Pinterest heights for Masonry Gallery items

--- src/slides/MasonryGallerySlide2.jsx
+++ src/slides/MasonryGallerySlide2.jsx
@@ -23,14 +23,14 @@ export default function MasonryGallerySlide2() {
         >
             <div style={{ width: "100%", height: "100%" }}>
                 <div className="masonry-grid" style={{ height: "100%" }}>
-                    <img className="masonry-item" src="/assets/images/mansory/pomelli-image(17).png" alt="Pomelli Image 17" />
-                    <img className="masonry-item" src="/assets/images/mansory/pomelli-image(18).png" alt="Pomelli Image 18" />
-                    <img className="masonry-item" src="/assets/images/mansory/pomelli-image(2).png" alt="Pomelli Image 2" />
-                    <img className="masonry-item" src="/assets/images/mansory/pomelli-image(19).png" alt="Pomelli Image 19" />
-                    <img className="masonry-item" src="/assets/images/mansory/pomelli-image(6).png" alt="Pomelli Image 6" />
-                    <img className="masonry-item" src="/assets/images/mansory/pomelli-image(8).png" alt="Pomelli Image 8" />
-                    <img className="masonry-item" src="/assets/images/mansory/9d63CUgb8OZ71v4sh7AOvn.png" alt="Pomelli Extra 1" />
-                    <img className="masonry-item" src="/assets/images/mansory/bVungMUNZ9z3OJdh5woONe.png" alt="Pomelli Extra 2" />
+                    <img className="masonry-item h-medium" src="/assets/images/mansory/pomelli-image(17).png" alt="Pomelli Image 17" />
+                    <img className="masonry-item h-tall" src="/assets/images/mansory/pomelli-image(18).png" alt="Pomelli Image 18" />
+                    <img className="masonry-item h-short" src="/assets/images/mansory/pomelli-image(2).png" alt="Pomelli Image 2" />
+                    <img className="masonry-item h-medium" src="/assets/images/mansory/pomelli-image(19).png" alt="Pomelli Image 19" />
+                    <img className="masonry-item h-tall" src="/assets/images/mansory/pomelli-image(6).png" alt="Pomelli Image 6" />
+                    <img className="masonry-item h-short" src="/assets/images/mansory/pomelli-image(8).png" alt="Pomelli Image 8" />
+                    <img className="masonry-item h-medium" src="/assets/images/mansory/9d63CUgb8OZ71v4sh7AOvn.png" alt="Pomelli Extra 1" />
+                    <img className="masonry-item h-tall" src="/assets/images/mansory/bVungMUNZ9z3OJdh5woONe.png" alt="Pomelli Extra 2" />
                 </div>
             </div>
         </section>
